d13ba95ee8f8e816762c8e86fae777ca6bf4fb83
jackottawa <tteng1992@gmail.com>
2018-03-07 19:39:01 -0500

    website skin

---
+++ Website Skin/skin.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+	// var oLink = document.getElementsByTagName("link")[0];
+	var oLink = document.getElementById("toggle");
+	var oSkin = document.getElementById("skin").getElementsByTagName("li");
+	for(var i = 0; i< oSkin.length; i++) {		
+		oSkin[i].onclick = function () {
+			for(var p in oSkin) 
+				oSkin[p].className = "";
+			this.className = "current";
+			oLink['href'] = "css/" + this.id + ".css";				
+		}	
+	}
+};
