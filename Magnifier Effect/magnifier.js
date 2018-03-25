d17c350aa2dc11bad5114e5d7157b1c82e46b110
jackottawa <tteng1992@gmail.com>
2018-03-25 16:48:41 -0400

    comments

--- Magnifier Effect/magnifier.js
+++ Magnifier Effect/magnifier.js
@@ -1,55 +1,61 @@
 window.onload = function(){
 
-			var normal = document.getElementById("normal");
-			var lay = document.getElementById("lay");
-			var bigImg = document.getElementById("bigImg");
-			var img = bigImg.getElementsByTagName("img")[0];
+	var normal = document.getElementById("normal");
+	var lay = document.getElementById("lay");
+	var bigImg = document.getElementById("bigImg");
+	var img = bigImg.getElementsByTagName("img")[0];
 
-			//鼠标移入时，将放大镜和bigImg显示出来 
-			normal.onmouseover = function(){
-				lay.style.display = "block";
-				bigImg.style.display = "block";
-			}
-			//鼠标移出时，将放大镜和bigImg隐藏 
-			normal.onmouseout = function(){
-				lay.style.display = "none";
-				bigImg.style.display = "none";
-			}
+	//Show the magnifier and bigImg when the mouse moves in
+	normal.onmouseover = function(){
+		lay.style.display = "block";
+		bigImg.style.display = "block";
+	}
+	//Hide the magnifier and bigImg when the mouse moves out
+	normal.onmouseout = function(){
+		lay.style.display = "none";
+		bigImg.style.display = "none";
+	}
 
-			normal.onmousemove = function( ev ){
-				var ev = ev || event;
-				var scale = 4; //图片的放缩比例 
-				//将鼠标放到放大镜的中间 
-				var x = ev.clientX - lay.offsetWidth/2 - normal.offsetLeft;
-				var y = ev.clientY - lay.offsetHeight/2 - normal.offsetTop;
-				//将放大镜的宽高与盒子的宽高结合起来按比例放缩  
-				lay.style.width = parseInt( normal.offsetWidth / scale ) + "px";
-				lay.style.height = parseInt( normal.offsetHeight / scale ) + "px";
-				//设置放大图片的宽高 
-				img.style.width = normal.offsetWidth * scale + "px";
-				img.style.height = normal.offsetHeight * scale + "px";
-				
-				//设置超出边界问题
-				if( x<0 ){
-					x = 0; //左边界的判断，当超出时将x置为0; 
-				}
-				if( x>=normal.offsetWidth - lay.offsetWidth ){
-					x = normal.offsetWidth - lay.offsetWidth; //右边界的判断，当超出时将x置为Box的宽度减去放大镜的宽度; 
-				}
-				if( y<0 ){
-					y = 0; //上边界的判断，当超出时将y置为0;
-				}
-				if( y>= normal.offsetHeight - lay.offsetHeight ){
-					y = normal.offsetHeight - lay.offsetHeight;  //下边界的判断，当超出时将y置为Box的高度减去放大镜的高度;
-				}
-				//放大镜随鼠标移动
-				lay.style.left = x + "px";
-				lay.style.top = y + "px";
-				//同比例放缩，大的盒子图片的放缩比例，当小盒子向右移动的时候，大盒子向左移动同等的比例的宽高，方向是相反的;
-				var left = scale * lay.offsetLeft;
-				var top = scale * lay.offsetTop;
-				img.style.left = -left + "px";
-				img.style.top = -top + "px";
-			}
-
+	normal.onmousemove = function( ev ){
+		var ev = ev || event;
+		var scale = 4; //Scale of picture 
+		//Place the mouse in the middle of the magnifier 
+		var x = ev.clientX - lay.offsetWidth/2 - normal.offsetLeft;
+		var y = ev.clientY - lay.offsetHeight/2 - normal.offsetTop;
+		// Combining the width and height of the magnifier with the box, 
+		// scaling up and down 
+		lay.style.width = parseInt( normal.offsetWidth / scale ) + "px";
+		lay.style.height = parseInt( normal.offsetHeight / scale ) + "px";
+		//Set the width and height of the enlarged image 
+		img.style.width = normal.offsetWidth * scale + "px";
+		img.style.height = normal.offsetHeight * scale + "px";
+		
+		//Setting out of bound problem
+		if( x<0 ){
+			x = 0; //Judgment of the left boundary, setting x to 0 when exceeded 
 		}
+		if( x>=normal.offsetWidth - lay.offsetWidth ){
+			x = normal.offsetWidth - lay.offsetWidth; 
+			//Judgment of the right boundary, when it exceeds, 
+			//x is set to the width of the box minus the width of the magnifier 
+		}
+		if( y<0 ){
+			y = 0; //Judgment on the upper boundary, set y to 0 when exceeded
+		}
+		if( y>= normal.offsetHeight - lay.offsetHeight ){
+			y = normal.offsetHeight - lay.offsetHeight;  
+			//The judgment of the lower boundary, when it exceeds, 
+			//set y to the height of Box minus the height of the magnifier
+		}
+		//Magnifier moves with the mouse
+		lay.style.left = x + "px";
+		lay.style.top = y + "px";
+		//When the small box moves to the right, the big box moves to the left
+		//by the same proportion of width and height, and the direction is opposite.
+		var left = scale * lay.offsetLeft;
+		var top = scale * lay.offsetTop;
+		img.style.left = -left + "px";
+		img.style.top = -top + "px";
+	}
+
+}
